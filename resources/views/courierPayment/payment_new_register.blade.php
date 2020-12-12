6606cdeea7ce824a773f139889ccb26fa935b229
Samuel Cruz <sam@ouvio.com>
2020-12-11 18:11:35 -0600

    VISTAS EN BLADE CORRIENDO

--- resources/views/courierPayment/payment_new_register.blade.php
+++ resources/views/courierPayment/payment_new_register.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>{{ $title }}</h1>
+                    <h1></h1>
                 </div>
             </div>
         </div>
@@ -24,76 +24,75 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Fecha:</label>
+                            <form action="{{ url('/pagosPorRepartidor') }}" method="POST">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Fecha:</label>
 
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
                                                 <span class="input-group-text"><i
                                                         class="far fa-calendar-alt"></i></span>
-                                            </div>
-                                            <input type="date" class="form-control" data-inputmask-alias="datetime"
-                                                   data-inputmask-inputformat="dd/mm/yyyy" data-mask>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Repartidor</label>
-                                        <select class="form-control select2" style="width: 100%;">
-                                            <option selected="selected">Adrián Hernández</option>
-                                            <option>Alejandro Gómez</option>
-                                            <option>Angel Ortiz</option>
-                                            <option>Antonio Arvizu</option>
-                                            <option>Arturo Cortés</option>
-                                            <option>Armando Peña</option>
-                                            <option>Arturo Austria Arteaga</option>
-                                            <option>Axel Martinez</option>
-                                            <option>Bernardo Aguilar</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Negocio</label>
-                                        <input type="text" class="form-control" placeholder="Autocomplete de negocios">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Número de Pedido</label>
-                                        <div class="input-group mb-3">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i
-                                                        class="fas fa-truck-loading"></i></span>
-                                            </div>
-                                            <input type="number" class="form-control" placeholder="No. Pedido">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Monto</label>
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text">$</span>
-                                            </div>
-                                            <input type="text" class="form-control">
-                                            <div class="input-group-append">
-                                                <span class="input-group-text">.00</span>
+                                                </div>
+                                                <input type="date" class="form-control" data-inputmask-alias="datetime"
+                                                       data-inputmask-inputformat="dd/mm/yyyy" data-mask name="fechaPago">
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Tipo de Pago</label>
-                                        <div class="input-group mb-3">
-                                            <select class="form-control select2" style="width: 100%;">
-                                                <option selected="selected">Efectivo</option>
-                                                <option>Tarjeta Bancaria</option>
+                                        <div class="form-group">
+                                            <label>Repartidor</label>
+                                            <select class="form-control select2" style="width: 100%;" name="repartidorPago">
+                                                @foreach ($couriers as $courier)
+                                                    <option value="{{ $courier[0] }}">{{ $courier[1] }}</option>
+                                                @endforeach
                                             </select>
                                         </div>
+                                        <div class="form-group">
+                                            <label>Negocio</label>
+                                            <input type="text" class="form-control"
+                                                   placeholder="Autocomplete de negocios" name="placesPagos">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Número de Pedido</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                <span class="input-group-text"><i
+                                                        class="fas fa-truck-loading"></i></span>
+                                                </div>
+                                                <input type="number" class="form-control" placeholder="No. Pedido" name="pedidoPago">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Monto</label>
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text">$</span>
+                                                </div>
+                                                <input type="text" class="form-control" name="montoPago">
+                                                <div class="input-group-append">
+                                                    <span class="input-group-text">.00</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Tipo de Pago</label>
+                                            <div class="input-group mb-3">
+                                                <select class="form-control select2" style="width: 100%;" name="tipoPagoPagos">
+                                                    @foreach ($paymentTypes as $paymentType)
+                                                        <option value="{{ $paymentType[0] }}">{{ $paymentType[1] }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <button type="button" class="btn btn-block btn-primary">Registrar Pago</button>
-                            </div>
+                                <div class="row">
+                                    <button type="button" class="btn btn-block btn-primary">Registrar Pago</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
--- resources/views/users/new_user.blade.php
+++ resources/views/users/new_user.blade.php
@@ -24,79 +24,86 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Nombre Completo</label>
-                                        <div class="input-group mb-3">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            <form action="{{ url('/usuarios') }}" method="POST">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Nombre Completo</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control" placeholder="Nombre y Apellido" name="nombreUsuarios">
                                             </div>
-                                            <input type="text" class="form-control" placeholder="Nombre y Apellido">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Status</label>
+                                            <select class="form-control select2" style="width: 100%;" name="statusUsuarios">
+                                                @foreach ($statuses as $status)
+                                                    <option value="{{ $status[0] }}">{{ $status[1] }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>¿Se le asigna terminal?</label>
+                                            <select class="form-control select2" style="width: 100%;" name="terminalUsuarios">
+                                                @foreach ($hasTerminals as $hasTerminal)
+                                                    <option value="{{ $hasTerminal[0] }}">{{ $hasTerminal[1] }}</option>
+                                                @endforeach
+                                            </select>
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <label>Status</label>
-                                        <select class="form-control select2" style="width: 100%;">
-                                            <option selected="selected">Activo</option>
-                                            <option>Inactivo</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>¿Se le asigna terminal?</label>
-                                        <select class="form-control select2" style="width: 100%;">
-                                            <option selected="selected">Sí</option>
-                                            <option>No</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Nombre Base de Datos</label>
-                                        <div class="input-group mb-3">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Nombre Base de Datos</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control" placeholder="Autocomplete" name="nombreBDUsuarios">
                                             </div>
-                                            <input type="text" class="form-control" placeholder="Autocomplete">
                                         </div>
-                                    </div>
-                                <div class="form-group">
-                                    <label>Correo Electrónico</label>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                        <div class="form-group">
+                                            <label>Correo Electrónico</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i
+                                                            class="fas fa-envelope"></i></span>
+                                                </div>
+                                                <input type="email" class="form-control"
+                                                       placeholder="Correo Electrónico" name="emailUsuarios">
+                                            </div>
                                         </div>
-                                        <input type="email" class="form-control" placeholder="Correo Electrónico">
-                                    </div>
-                                </div>
 
-                                <div class="form-group">
-                                    <label>Número de Terminal</label>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="fas fa-list-ol"></i></span>
+                                        <div class="form-group">
+                                            <label>Número de Terminal</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="fas fa-list-ol"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control"
+                                                       placeholder="Número de terminal" name="numeroTerminalUsuarios">
+                                            </div>
                                         </div>
-                                        <input type="text" class="form-control" placeholder="Número de terminal">
                                     </div>
                                 </div>
-                            </div>
-                        </div>    
-                        <div class="row">
-                            <button type="button" class="btn btn-block btn-primary">Registrar Usuario</button>
+                                <div class="row">
+                                    <button type="button" class="btn btn-block btn-primary">Registrar Usuario</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>  
     </section>
 
 
 @section('script')
 
     <script type="text/javascript">
-        $(document).ready(function () 
-        {
-            
+        $(document).ready(function () {
+
         });
     </script>
 
